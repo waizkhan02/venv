82986cd0889bf13b45a293525abbb959a2f78f45
waizkhan02 <waizr8697@gmail.com>
2026-01-06 12:48:03 +0500

    changes make at jan 6

--- addons/my_library/__manifest__.py
+++ addons/my_library/__manifest__.py
@@ -1,26 +1,28 @@
 {'name': "My library",
-'summary': "Manage books easily",
-'description': """
+ 'summary': "Manage books easily",
+ 'description': """
 Manage Library
 ==============
 Description related to library.
 """,
-'author': "waiz khan",
-'website': "http://www.example.com",
-'category': 'Uncategorized',
-'version': '14.0.1.0.0',
-'depends': ['contacts'],
-'data': [
-    'security/Groups.xml',
-    'security/ir.model.access.csv',
-    'views/library_book.xml',
-    'views/library_book_category_views.xml',
-    'views/res_partner_view.xml',
-    'views/library_member.xml',
-    'views/library_book_rent.xml',
-    'wizard/library_book_rent_wizard.xml',
-    'wizard/return_wizard.xml',
-    # 'security/library_rules.xml',
-    ],
+ 'author': "waiz khan",
+ 'website': "http://www.example.com",
+ 'category': 'Uncategorized',
+ 'version': '14.0.1.0.0',
+ 'depends': ['contacts'],
+ 'data': [
+     'security/Groups.xml',
+     'security/ir.model.access.csv',
+     'views/library_book.xml',
+     'views/library_book_category_views.xml',
+     'views/res_partner_view.xml',
+     'views/library_member.xml',
+     'views/library_book_rent.xml',
+     'wizard/library_book_rent_wizard.xml',
+     'wizard/return_wizard.xml',
+     'views/library.book.rent.statistics.xml',
+     'views/res_setting_config.xml',
+     # 'security/library_rules.xml',
+ ],
  # 'sequence' : -80,
  }
--- addons/my_library/models/__init__.py
+++ addons/my_library/models/__init__.py
@@ -2,3 +2,5 @@ from . import library_book
 from . import library_book_category
 from . import library_member
 from . import library_book_rent
+from . import library_book_rent_statistics
+from . import res_setting_config
